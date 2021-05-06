5c650d00144bf277cb731f268577ae8e94f9e80e
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-05-07 02:09:34 +0800

    Commit all

--- opt/panel/index.php
+++ opt/panel/index.php
@@ -17,7 +17,7 @@ if (!empty($_SESSION['user']) && $user = user_info($_SESSION['user'])) {
 ?><!doctype html>
 <html>
 <head>
-	<title>MCHostPanel | Login</title>
+	<title>SPICECRAFT | LOGIN CPANEL</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
